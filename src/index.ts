e3ebaa0f685018e500048c3010d656b63412bbad
dominic <dominicf2001@gmail.com>
2024-04-20 12:45:53 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia";
+import Database from "bun:sqlite";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
