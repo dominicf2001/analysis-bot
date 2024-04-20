c8f08c233f130ed1a07629268736caed90ab2cdd
dominic <dominicf2001@gmail.com>
2024-04-20 13:35:16 -0400

    init db work

---
+++ src/db/initDb.ts
@@ -0,0 +1,71 @@
+import Database from "bun:sqlite";
+import { parseArgs } from "util";
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: {
+    f: {
+      type: 'boolean',
+    },
+  },
+  strict: true,
+  allowPositionals: true
+});
+
+const db = new Database("db.sqlite");
+
+// delete tables
+if (values.f){
+    console.log("Dropping tables...");
+    const tableNames = ["users", "types_big_five", "types_f", "types_phil"];
+    tableNames.forEach(tableName => {
+      db.query(`DROP TABLE IF EXISTS ${tableName}`).run();
+    });
+}
+
+
+console.log("Creating tables...");
+
+// Recreate tables
+db.query(`CREATE TABLE IF NOT EXISTS users (
+  username TEXT PRIMARY KEY
+)`).run();
+
+// BIG FIVE
+db.query(`CREATE TABLE IF NOT EXISTS types_big_five (
+  username TEXT,
+  neuro INTEGER,
+  extrav INTEGER,
+  open INTEGER,
+  agree INTEGER,
+  con INTEGER,
+  FOREIGN KEY(username) REFERENCES users(username)
+)`).run();
+
+// F TYPES
+db.query(`CREATE TABLE IF NOT EXISTS types_f (
+  username TEXT,
+  a INTEGER,
+  b INTEGER,
+  c INTEGER,
+  d INTEGER,
+  FOREIGN KEY(username) REFERENCES users(username)
+)`).run();
+
+// PHIL TYPES
+db.query(`CREATE TABLE IF NOT EXISTS types_phil (
+  username TEXT,
+  stoic INTEGER,
+  nihil INTEGER,
+  absurd INTEGER,
+  exist INTEGER,
+  empir INTEGER,
+  rational INTEGER,
+  conf INTEGER,
+  determine INTEGER,
+  object INTEGER,
+  util INTEGER,
+  FOREIGN KEY(username) REFERENCES users(username)
+)`).run();
+
+console.log("Finished...");
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,3 @@ const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-
-const db = new Database("db.sqlite");
-db.query("CREATE TABLE test (valOne, valTwo);").run();
-db.close();
