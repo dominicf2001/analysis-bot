45f9c12556fd6a01769afc7f18e6cbff341499f0
gwstallsmith <gwstallsmith@gmail.com>
2024-04-21 06:21:53 -0400

    Scraper functions are now one function.

--- scraper_api.py
+++ scraper_api.py
@@ -2,14 +2,13 @@ import scrape_to_json as scrape_to_json
 import process_messages as process_messages
 import evaluate_messages as evaluate_messages
 
+def get_chat_evaluation(user_id):
+    scrape_to_json.scrape_to_json()
 
-scrape_to_json.scrape_to_json()
+    json_data = process_messages.load_json_file()
 
-json_data = process_messages.load_json_file()
-user_id = "big_gert"
+    process_messages.tokens_to_csv(process_messages.process_user_messages(user_id, json_data), 'sentiment_evaluation.csv')
 
-process_messages.tokens_to_csv(process_messages.process_user_messages(user_id, json_data), 'sentiment_evaluation.csv')
-
-report = evaluate_messages.evaluate_data()
-final_count = evaluate_messages.count_evaluations(report)
-print(final_count)
+    report = evaluate_messages.evaluate_data()
+    final_count = evaluate_messages.count_evaluations(report)
+    print(final_count)
