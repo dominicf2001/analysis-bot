77b640e42f02089a0d6324f08bec36e0c8874845
awkward0001 <jdemore4@kent.edu>
2024-04-20 22:22:39 -0400

    implrmrny bot

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -36,7 +36,7 @@ export const Layout = (props: LayoutProps & elements.Children) => {
             </header>
             <main>
               <section id="introSection">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p>Hello Test Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
               </section>
               <section>
                 <button id="inviteButton">
